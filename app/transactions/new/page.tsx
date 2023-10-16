0ee6f61f1c7343f3a5d9a87c04f988d775afd9ea
Tim <prosight11@gamil.com>
2023-10-16 22:13:31 +0200

    markdown editor

--- app/transactions/new/page.tsx
+++ app/transactions/new/page.tsx
@@ -1,33 +1,30 @@
-'use client'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+import { Button, TextField } from "@radix-ui/themes";
+import React from "react";
+import SimpleMDE from 'react-simplemde-editor'
+import 'easymde/dist/easymde.min.css'
 
-type Props = {}
+type Props = {};
 
 function page({}: Props) {
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root>
-          <TextField.Input placeholder='Summary' />
-
-        </TextField.Root>
-        <TextField.Root>
-          <TextField.Input placeholder='Category' />
-
-        </TextField.Root>
-        <TextField.Root>
-          <TextField.Input placeholder='Currency' />
-
-        </TextField.Root>
-        <TextField.Root>
-          <TextField.Input placeholder='Sum' />
-
-        </TextField.Root>
-        <TextArea placeholder='Description'>
-          </TextArea> 
-          <Button>Submit new Expense</Button>
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="Summary" />
+      </TextField.Root>
+      <TextField.Root>
+        <TextField.Input placeholder="Category" />
+      </TextField.Root>
+      <TextField.Root>
+        <TextField.Input placeholder="Currency" />
+      </TextField.Root>
+      <TextField.Root>
+        <TextField.Input placeholder="Sum" />
+      </TextField.Root>
+      <SimpleMDE placeholder="Description"></SimpleMDE>
+      <Button>New Transaction</Button>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
