46b49305acd26b498ff442aa1470791e3f769e0f
Your Name <you@example.com>
2023-10-16 12:56:46 +0200

    style active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,16 @@
+'use client'
+import classNames from 'classnames'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { FaCoins} from 'react-icons/fa'
+import classnames from 'classnames'
 
 type Props = {}
 
 function NavBar({}: Props) {
+
+    const path = usePathname()
 
     const links = [
         { label: 'Dashboard', href:'/'},
@@ -16,7 +22,13 @@ function NavBar({}: Props) {
         <p>sprintForm</p>
         <ul className='flex space-x-6'>
             {
-                links.map(link => <Link key={link.href} className={} href={link.href}>{link.label}</Link>)
+                links.map(link => <Link key={link.href} className={classnames(
+                    {
+                        'text-zinc-900': link.href === path,
+                        'text-zinc-500': link.href !== path,
+                        'hover:text-zinc-800 transition-colors': true
+                    }
+                )} href={link.href}>{link.label}</Link>)
 
             }
         </ul>
---
+++ app/transactions/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function TransactionsPage({}: Props) {
+  return (
+    <div>TPage</div>
+  )
+}
+
+export default TransactionsPage
