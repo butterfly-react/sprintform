a5a5f7f8949c3de013b9be973e7c77df33ab6df6
Your Name <you@example.com>
2023-10-17 13:25:23 +0200

    dropdown menu

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,53 +1,79 @@
-'use client'
-import classNames from 'classnames'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
-import { FaCoins} from 'react-icons/fa'
-import classnames from 'classnames'
-import { useSession } from 'next-auth/react'
-import { Box } from '@radix-ui/themes'
+"use client";
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { FaCoins } from "react-icons/fa";
+import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import { Avatar, Box, Container, DropdownMenu, Flex } from "@radix-ui/themes";
 
-type Props = {}
+type Props = {};
 
 function NavBar({}: Props) {
+  const path = usePathname();
+  const { status, data: session } = useSession();
 
-    const path = usePathname()
-    const { status, data: session} = useSession()
-
-    const links = [
-        { label: 'Dashboard', href:'/'},
-        { label: 'Transactions', href:'/transactions'},
-    ]
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Transactions", href: "/transactions" },
+  ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href='/'><FaCoins /> </Link>
-        <p>sprintForm</p>
-        <ul className='flex space-x-6'>
-            {
-                links.map(link => 
-                    <li key={link.href}>
+    <nav className=" border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <FaCoins />{" "}
+            </Link>
+            <p>sprintForm</p>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classnames({
+                      "text-zinc-900": link.href === path,
+                      "text-zinc-500": link.href !== path,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
 
-                        <Link  className={classnames(
-                            {
-                                'text-zinc-900': link.href === path,
-                                'text-zinc-500': link.href !== path,
-                                'hover:text-zinc-800 transition-colors': true
-                            }
-                        )} href={link.href}>{link.label}</Link>
-                    </li>
-                
-                )
+          <Box>
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
 
-            }
-        </ul>
-        <Box>
-            { status === 'authenticated' && <Link href='/api/auth/signout'>Logout</Link>}
-            { status === 'unauthenticated' && <Link href='/api/auth/signin'>Login</Link>}
-
-        </Box>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>{session.user!.email}</DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Logout</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
